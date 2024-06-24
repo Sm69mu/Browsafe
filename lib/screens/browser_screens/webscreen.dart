2addf0f66033eb61e64b75688dfb08f0d78a0bec
Soumyajit Mukherjee <soumyajitmukherjee271@gmail.com>
2024-06-24 21:28:27 +0530

    .

--- lib/screens/browser_screens/webscreen.dart
+++ lib/screens/browser_screens/webscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import "package:webview_flutter/webview_flutter.dart";
 
 class WebScreen extends StatefulWidget {
   final String url;
